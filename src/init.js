fd40db929f363b920f9c8a79b23ca3766a1dea9c
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-22 10:27:52 +0000

    Add and style add task icon

--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
 import displayProject from './dom.js';
 import addAllListeners from './addEventListeners.js';
 import menu from './assets/icons/menu_open.svg';
-
+import add from './assets/icons/add.svg';
 
 function initialize(project) {
     displayProject(project);
@@ -11,11 +11,23 @@ function initialize(project) {
 }
 
 function displayIcons() {
+    displayMenuIcon();
+    displayAddIcon();
+}
+
+function displayMenuIcon() {
     const collapseBtn = document.querySelector(".collapse-btn");
     const icon = document.createElement("img");
     icon.src = menu;
     icon.classList.add("menu-icon");
     collapseBtn.append(icon);
+}
+
+function displayAddIcon() {
+    const addBtn = document.querySelector(".add-btn");
+    const icon = document.createElement("img");
+    icon.src = add;
+    addBtn.prepend(icon);
 }
 
 export default initialize;
